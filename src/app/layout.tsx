a41fd92a7d7dfb47d15e3109b705bb8038955847
hicham gaddari <gaddariowen@gmail.com>
2024-07-14 14:08:42 +0100

    add g-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
-
+import GoogleAnalytics from "./components/GoogleAnalytics";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +18,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`
+        }}
+      >
+
+      </Script>
       <Analytics />
+      <GoogleAnalytics />
+
       <body className={inter.className}>{children}</body>
     </html>
   );
